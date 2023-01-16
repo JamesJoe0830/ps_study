35dd7246458680b7cabc44bd4cdc3971af2348dd
joejames0830 <kyeun950830@gmail.com>
2023-01-16 23:07:33 +0900

    while_practice.js

---
+++ week2/while_pracitce.js
@@ -0,0 +1,11 @@
+documents.queryselect('a') 
+var alist = document.querySelectorAll('a'); 
+consol.log() 
+
+var alist =document.queryselectorAll('a');
+var i=0;
+while(i < alist.length){
+		console log(alist[i]); 
+		alist[i].style.color  = 'powderblue';
+		i ++;
+}
