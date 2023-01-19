216b0b54d4b48db5cb76c6f1d9b694964845d12b
joejames0830 <kyeun950830@gmail.com>
2023-01-19 12:01:38 +0900

    string_in_string.js

---
+++ week2/string_in_string.js
@@ -0,0 +1,12 @@
+function solution(str1, str2) {
+    var answer = 0;
+    if(str1.indexOf(str2)>=0){
+        answer=1;
+    }
+    else{
+        answer=2;
+    }
+    console.log(str1.search(str2))
+    return answer;
+    
+}
