0ef34ed13f3a272e2f0024ba73046d95fc0d1b6a
joejames0830 <kyeun950830@gmail.com>
2023-04-10 23:35:43 +0900

    1012(유기농 배추).py recursion DFS

--- BFS_DFS/1012(유기농_배추).py
+++ BFS_DFS/1012(유기농_배추).py
@@ -1,9 +1,11 @@
+# https://www.acmicpc.net/problem/1012 (유기농 배추) 실버2
+
 import sys
 sys.setrecursionlimit(10000) #재귀 한도를 1000까지 풀어줌 (파이썬에서 재귀 한도가 시스템안정을 위해서 정해져있기 때문에)
 
 def DFS(x,y):
     if x<0 or x>=N or y<0 or y>=M: # graph범위 넘어가면 재귀함수 종료
-        return False
+        return False    
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
     if graph[x][y] == 1 and visited[x][y] == 0:
