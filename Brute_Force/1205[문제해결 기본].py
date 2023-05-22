5c732356f860d9e2e4f64116a254bda4318fc8f6
joejames0830 <kyeun950830@gmail.com>
2023-05-22 13:49:13 +0900

    1205 [문제해결 기본]

---
+++ Brute_Force/1205[문제해결 기본].py
@@ -0,0 +1,12 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV134DPqAA8CFAYh 
+# 
+# 
+# 
+# -----------------------------------------------------------------------------
+import sys
+input = sys.stdin.readline
+
+for _ in range(10):
+    N=int(input())
+    for n in range(N):
+        
